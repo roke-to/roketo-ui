04a516f32ae4093cff7daa2b18fd0af53414cefa
Goshan <george@near.money>
2021-09-03 20:59:06 +0300

    add tailwinds.css

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [],
+  prefix: 'tw',
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
