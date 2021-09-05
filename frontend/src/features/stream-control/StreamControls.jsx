d6a204a575e0ef2f89d28347a9bdd5d8b4c9c30a
Alex Kouprin <kpr@nearprotocol.com>
2021-09-05 22:09:19 +0100

    INITIALIZED status

--- frontend/src/features/stream-control/StreamControls.jsx
+++ frontend/src/features/stream-control/StreamControls.jsx
@@ -1,38 +1,38 @@
-import React from 'react';
-import { useStreamControl } from './useStreamControl';
+import React from 'react'
+import { useStreamControl } from './useStreamControl'
 
-export function StreamControls({ output }) {
-  const controls = useStreamControl(output.stream_id);
+export function StreamControls ({ output }) {
+  const controls = useStreamControl(output.stream_id)
 
   if (controls.loading) {
     // TODO: cool loader
-    return <span>Loading!</span>;
+    return <span>Loading!</span>
   }
 
-  return output.status === 'ACTIVE' || output.status === 'PAUSED' ? (
-    <div className="d-flex flex-row">
+  return output.status === 'ACTIVE' || output.status === 'PAUSED' || output.status === 'INITIALIZED' ? (
+    <div className='d-flex flex-row'>
       <div>
         {output.status === 'ACTIVE' ? (
           <button
-            type="button"
-            className="btn btn-warning btn-sm m-1"
+            type='button'
+            className='btn btn-warning btn-sm m-1'
             onClick={controls.pause}
           >
             Pause
           </button>
         ) : (
           <button
-            type="button"
-            className="btn btn-warning btn-sm m-1"
+            type='button'
+            className='btn btn-warning btn-sm m-1'
             onClick={controls.restart}
           >
-            Restart
+            Start
           </button>
         )}
       </div>
       <button
-        type="button"
-        className="btn btn-danger btn-sm m-1"
+        type='button'
+        className='btn btn-danger btn-sm m-1'
         onClick={controls.stop}
       >
         Stop
@@ -40,5 +40,5 @@ export function StreamControls({ output }) {
     </div>
   ) : (
     <div />
-  );
+  )
 }
