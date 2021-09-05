0c027051d09777f87f3bd1d1e2feeb2631d5552c
Alex Kouprin <kpr@nearprotocol.com>
2021-09-05 19:38:34 +0100

    upd frontend to work with new version of the contract

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,42 @@
-import React from 'react';
-import 'error-polyfill';
-import 'bootstrap/dist/js/bootstrap.bundle';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.scss';
+import React from 'react'
+import 'error-polyfill'
+import 'bootstrap/dist/js/bootstrap.bundle'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.scss'
 import {
   HashRouter as Router,
   Link,
   Route,
   Switch,
-  Redirect,
-} from 'react-router-dom';
-import SendPage from './pages/Send';
-import ReceivePage from './pages/Receive';
-import { Header } from './components/Header';
-import { NearContext, useCreateNear } from './features/near-connect/useNear';
-import { MyStreamsPage } from './pages/MyStreams';
-import { AuthorizePage } from './pages/Authorize';
+  Redirect
+} from 'react-router-dom'
+import SendPage from './pages/Send'
+import ReceivePage from './pages/Receive'
+import { Header } from './components/Header'
+import { NearContext, useCreateNear } from './features/near-connect/useNear'
+import { MyStreamsPage } from './pages/MyStreams'
+import { AuthorizePage } from './pages/Authorize'
 
 // A wrapper for <Route> that redirects to the login
 // screen if you're not yet authenticated.
-function PrivateRoute({ children, allowed, redirect, ...rest }) {
+function PrivateRoute ({ children, allowed, redirect, ...rest }) {
   return (
     <Route
       {...rest}
       render={({ location }) => {
-        console.log(`Render ${rest.path}, allowed? ${allowed}`);
-        return allowed ? children : redirect;
+        console.log(`Render ${rest.path}, allowed? ${allowed}`)
+        return allowed ? children : redirect
       }}
     />
-  );
+  )
 }
 
-function AppFn() {
-  const near = useCreateNear();
+function AppFn () {
+  const near = useCreateNear()
 
   return (
     <NearContext.Provider value={near}>
-      <div className="twind-bg-dark text-white">
+      <div className='twind-bg-dark text-white'>
         {near.inited ? (
           <Router basename={process.env.PUBLIC_URL}>
             <Header />
@@ -44,34 +44,34 @@ function AppFn() {
             <Switch>
               <PrivateRoute
                 exact
-                redirect={<Redirect to="/" />}
+                redirect={<Redirect to='/' />}
                 allowed={!near.auth.signedIn}
-                path="/authorize"
+                path='/authorize'
               >
                 <AuthorizePage />
               </PrivateRoute>
 
               <PrivateRoute
                 exact
-                redirect={<Redirect to="/authorize" />}
+                redirect={<Redirect to='/authorize' />}
                 allowed={near.auth.signedIn}
-                path="/"
+                path='/'
               >
                 <SendPage />
               </PrivateRoute>
               <PrivateRoute
                 exact
-                redirect={<Redirect to="/authorize" />}
+                redirect={<Redirect to='/authorize' />}
                 allowed={near.auth.signedIn}
-                path="/my_streams"
+                path='/my_streams'
               >
                 <MyStreamsPage />
               </PrivateRoute>
               <PrivateRoute
                 exact
-                redirect={<Redirect to="/authorize" />}
+                redirect={<Redirect to='/authorize' />}
                 allowed={near.auth.signedIn}
-                path="/receive"
+                path='/receive'
               >
                 <ReceivePage />
               </PrivateRoute>
@@ -80,7 +80,7 @@ function AppFn() {
         ) : null}
       </div>
     </NearContext.Provider>
-  );
+  )
 }
 
-export default AppFn;
+export default AppFn
--- frontend/src/features/near-connect/useNear.js
+++ frontend/src/features/near-connect/useNear.js
@@ -1,25 +1,25 @@
-import React, { useContext, useEffect, useState } from 'react';
-import * as nearAPI from 'near-api-js';
-import { NearContractApi } from './near-contract-api';
+import React, { useContext, useEffect, useState } from 'react'
+import * as nearAPI from 'near-api-js'
+import { NearContractApi } from './near-contract-api'
 
-const IsMainnet = window.location.hostname === '(xyiming)'; // TODO
+const IsMainnet = window.location.hostname === '(xyiming)' // TODO
 const TestNearConfig = {
   accountSuffix: 'testnet',
   networkId: 'testnet',
   nodeUrl: 'https://rpc.testnet.near.org',
-  contractName: 'dev-1630685656410-62108694435619',
+  contractName: 'dev-1630866505805-59948878430656',
   walletUrl: 'https://wallet.testnet.near.org',
-  ft: 'dev-1630798753809-34755859843881',
-};
+  ft: 'dev-1630798753809-34755859843881'
+}
 const MainNearConfig = {
   accountSuffix: 'near',
   networkId: 'mainnet',
   nodeUrl: 'https://rpc.mainnet.near.org',
-  contractName: 'dev-1630411495814-97749356114440',
-  walletUrl: 'https://wallet.near.org',
-};
+  contractName: 'dev-1630866505805-59948878430656',
+  walletUrl: 'https://wallet.near.org'
+}
 
-export const NearConfig = IsMainnet ? MainNearConfig : TestNearConfig;
+export const NearConfig = IsMainnet ? MainNearConfig : TestNearConfig
 
 export const NearContext = React.createContext({
   inited: false,
@@ -29,23 +29,23 @@ export const NearContext = React.createContext({
     walletConnection: null,
     accountId: null,
     contract: null,
-    ft: null,
+    ft: null
   },
   auth: {
     signedIn: false,
-    signedAccountId: null,
+    signedAccountId: null
   },
   contractApi: NearContractApi({}),
   refreshAllowance: () => {},
   login: () => {},
-  logout: () => {},
-});
+  logout: () => {}
+})
 
-async function createNearInstance() {
-  const keyStore = new nearAPI.keyStores.BrowserLocalStorageKeyStore();
+async function createNearInstance () {
+  const keyStore = new nearAPI.keyStores.BrowserLocalStorageKeyStore()
   const near = await nearAPI.connect(
-    Object.assign({ deps: { keyStore } }, NearConfig),
-  );
+    Object.assign({ deps: { keyStore } }, NearConfig)
+  )
 
   const _near = {
     keyStore: null,
@@ -53,19 +53,19 @@ async function createNearInstance() {
     walletConnection: null,
     accountId: null,
     contract: null,
-    ft: null,
-  };
+    ft: null
+  }
 
-  _near.keyStore = keyStore;
-  _near.near = near;
+  _near.keyStore = keyStore
+  _near.near = near
 
   _near.walletConnection = new nearAPI.WalletConnection(
     near,
-    NearConfig.contractName,
-  );
-  _near.accountId = _near.walletConnection.getAccountId();
+    NearConfig.contractName
+  )
+  _near.accountId = _near.walletConnection.getAccountId()
 
-  _near.account = _near.walletConnection.account();
+  _near.account = _near.walletConnection.account()
   _near.contract = new nearAPI.Contract(
     _near.account,
     NearConfig.contractName,
@@ -80,75 +80,75 @@ async function createNearInstance() {
         'create_bridge',
         'delete_bridge',
         'push_flow',
-        'stop_stream',
-      ],
-    },
-  );
+        'stop_stream'
+      ]
+    }
+  )
 
   // TODO set multiple
   _near.ft = new nearAPI.Contract(_near.account, NearConfig.ft, {
     viewMethods: ['ft_balance_of'],
-    changeMethods: ['ft_transfer', 'ft_transfer_call'],
-  });
+    changeMethods: ['ft_transfer', 'ft_transfer_call']
+  })
 
-  return _near;
+  return _near
 }
 
-export function useCreateNear() {
-  let apiRef = React.useRef(null);
+export function useCreateNear () {
+  const apiRef = React.useRef(null)
 
-  let [inited, setInited] = useState(false);
-  let [near, setNear] = useState({
+  const [inited, setInited] = useState(false)
+  const [near, setNear] = useState({
     keyStore: null,
     near: null,
     walletConnection: null,
     accountId: null,
     contract: null,
-    ft: null,
-  });
+    ft: null
+  })
 
   const auth = {
     signedIn: !!near.accountId,
-    signedAccountId: near.accountId,
-  };
+    signedAccountId: near.accountId
+  }
 
-  async function login() {
-    const appTitle = 'Streaming Xyiming';
+  async function login () {
+    const appTitle = 'Streaming Xyiming'
 
     await near.walletConnection.requestSignIn(
       NearConfig.contractName,
-      appTitle,
-    );
+      appTitle
+    )
   }
 
-  function logout() {
-    near.walletConnection.signOut();
+  function logout () {
+    near.walletConnection.signOut()
 
     setNear({
       ...near,
-      accountId: null,
-    });
+      accountId: null
+    })
   }
 
-  async function refreshAllowance() {
+  async function refreshAllowance () {
     alert(
-      "You're out of access key allowance. Need sign in again to refresh it",
-    );
-    await logout();
-    //await requestSignIn();
+      "You're out of access key allowance. Need sign in again to refresh it"
+    )
+    await logout()
+    // await requestSignIn();
   }
 
   useEffect(() => {
     const init = async () => {
-      const _near = await createNearInstance();
+      const _near = await createNearInstance()
 
-      apiRef.current = NearContractApi(_near);
-      setNear(_near);
-      setInited(true);
-    };
+      apiRef.current = NearContractApi(_near)
+      setNear(_near)
+      setInited(true)
+    }
 
-    init();
-  }, []);
+    init()
+  }, [])
 
   return {
     auth,
@@ -157,19 +157,19 @@ export function useCreateNear() {
     inited,
     login,
     logout,
-    refreshAllowance,
-  };
+    refreshAllowance
+  }
 }
 
-const ERR_NOT_IN_NEAR_CONTEXT = new Error('Near context is not found');
+const ERR_NOT_IN_NEAR_CONTEXT = new Error('Near context is not found')
 
-export function useNear() {
-  const near = useContext(NearContext);
-  const insideContext = !!near;
+export function useNear () {
+  const near = useContext(NearContext)
+  const insideContext = !!near
 
   if (!insideContext) {
-    throw ERR_NOT_IN_NEAR_CONTEXT;
+    throw ERR_NOT_IN_NEAR_CONTEXT
   }
 
-  return near;
+  return near
 }
