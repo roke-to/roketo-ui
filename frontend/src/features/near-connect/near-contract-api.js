1fffc9a1f6a33bd7ddad5be7415cd09c94b8c7a5
Alex Kouprin <kpr@nearprotocol.com>
2021-09-07 15:27:32 +0100

    stream view fix

--- frontend/src/features/near-connect/near-contract-api.js
+++ frontend/src/features/near-connect/near-contract-api.js
@@ -15,11 +15,25 @@ export function NearContractApi(near) {
     return [];
   };
 
+  const getStream = async (...args) => {
+    try {
+      console.log('???', args[1]);
+      if (!args[1]) {
+        return null; // TODO ?
+      }
+      return await contract.get_stream({stream_id: args[1]});
+    } catch (e) {
+      console.log('near error', e);
+    }
+    return null;
+  };
+
   const getCurrentAccount = () =>
     getAccount(near.walletConnection.getAccountId());
 
   return {
     getCurrentAccount,
     getAccount,
+    getStream,
   };
 }
