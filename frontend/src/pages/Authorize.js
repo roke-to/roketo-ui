f8ec0f4f9dc16700734cb39a712f8f0185fdc12d
Kirill Arutyunov <kirill@arutyunov.me>
2021-09-10 20:38:05 +0300

    New login page

--- frontend/src/pages/Authorize.js
+++ frontend/src/pages/Authorize.js
@@ -7,15 +7,25 @@ export function AuthorizePage() {
   const near = useNear();
 
   return (
-    <div className="twind-my-24 twind-text-center">
-      <h1 className="twind-text-3xl twind-mb-4 twind-font-semibold">
-        {' '}
-        Login via NEAR Network{' '}
-      </h1>
-      <p className="twind-text-gray">
-        Click button below to login or create new account
-      </p>
-      <NearAuthButton className="twind-mt-10" />
+    <div className="twind-flex twind-h-screen">
+      <div
+        className="lg:twind-m-auto twind-mx-4 twind-my-auto lg:twind-w-2/5 twind-bg-gray-90 twind-rounded-3xl twind-p-5"
+        style={{'background-color': '#0D0B26'}}
+      >
+        <div className="twind-my-24 twind-text-center">
+          <h1 className="twind-text-3xl twind-mb-4 twind-font-semibold">
+            {' '}
+            Login via NEAR Network{' '}
+          </h1>
+          <p className="twind-text-gray">
+            Click button below to login or create new account
+          </p>
+          <NearAuthButton
+            variant="main"
+            className="twind-mt-10 twind-rounded-lg twind-py-4 twind-px-8"
+          />
+        </div>
+      </div>
     </div>
   );
 }
