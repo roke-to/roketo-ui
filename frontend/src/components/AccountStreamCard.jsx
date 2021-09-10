feb39ac2971bda23016062c86609c628e603107f
Kirill Arutyunov <kirill@arutyunov.me>
2021-09-10 14:10:35 +0300

    Small visual fixes for account page

--- frontend/src/components/AccountStreamCard.jsx
+++ frontend/src/components/AccountStreamCard.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {tokens, TokenFormatter} from '../lib/formatting';
 import {utils} from 'near-api-js';
 import numbro from 'numbro';
+import {Tokens} from './icons';
 
 export function AccountStreamCard({
   token,
   balance,
   streamsLength,
   period = '',
+  showPeriod = true,
 }) {
   const formatter = TokenFormatter(token);
 
@@ -29,22 +31,30 @@ export function AccountStreamCard({
   balance = balance * multiplier;
 
   return (
-    <div className="twind-w-full twind-h-24 twind-rounded-lg twind-bg-card2 twind-flex twind-items-center twind-width-full twind-p-6 twind-mr-4">
+    <div className="twind-w-full twind-h-24 twind-rounded-lg twind-bg-input twind-flex twind-items-center twind-width-full twind-p-6 twind-mr-4">
       <div className="twind-w-full twind-flex twind-items-center">
-        <div className="twind-w-12 twind-mr-12">icon</div>
+        <div className="twind-w-12 twind-mr-12">
+          <span className="twind-flex-shrink-0 twind-rounded-full twind-bg-card2 twind-inline-flex twind-items-center twind-justify-center twind-w-12 twind-h-12">
+            {Tokens(token)}
+          </span>
+        </div>
         <div className="">
           <div className="twind-font-bold">
             {tokens[token].name}, {token}
           </div>
-          <div className="twind-text-gray twind-text-sm">
-            from {streamsLength} steams
-          </div>
+          {streamsLength > 0 ? (
+            <div className="twind-text-gray twind-text-sm">
+              from {streamsLength} steams
+            </div>
+          ) : (
+            ''
+          )}
         </div>
         <div className="twind-ml-auto">
           <span className=" twind-text-3xl">
             {balance < 0.001 ? '<0.001' : numbro(balance).format({mantissa: 3})}
           </span>
-          <span>{period !== '' ? '/' + period : ''}</span>
+          {showPeriod ? <span>{period !== '' ? '/' + period : ''}</span> : ''}
         </div>
       </div>
     </div>
