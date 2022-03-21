5f6fc8e9af3a338caa69aaa81673c6ac703a3cbb
Serge K Lebedev <hello@sergelebe.dev>
2022-03-22 02:17:10 +0600

    Refactor streamViewData a bit

--- src/features/stream-view/streamViewData.ts
+++ src/features/stream-view/streamViewData.ts
@@ -3,33 +3,30 @@ import type { RoketoStream } from 'shared/api/roketo/interfaces/entities';
 import type { TokenFormatter } from 'shared/helpers/formatting';
 
 export function streamViewData(stream: RoketoStream, tokenFormatter: ReturnType<typeof TokenFormatter>) {
-  const tf = tokenFormatter;
-
   // public link
   const link = `${window.location.origin}/#/streams/${stream.id}`;
 
   // time left calculations
-  const secondsLeft = tf.ticksToMs(
+  const microsecondsLeft = tokenFormatter.ticksToMs(
     Math.round(
       (Number(stream.balance) - Number(stream.available_to_withdraw)) / Number(stream.tokens_per_tick),
     ),
   );
 
-  const dateEnd = new Date(new Date().getTime() + secondsLeft);
+  const dateEnd = new Date(new Date().getTime() + microsecondsLeft);
   const timestampEnd = dateEnd.getTime();
 
   // progress bar calculations
   const full = Number(stream.balance) + Number(stream.tokens_total_withdrawn);
   const withdrawn = Number(stream.tokens_total_withdrawn);
-  const streamed = Number(stream.tokens_total_withdrawn)
-    + Number(stream.available_to_withdraw);
   const available = Number(stream.available_to_withdraw);
+  const streamed = withdrawn + available;
 
   const left = full - streamed;
   const progresses = [withdrawn / full, streamed / full];
 
   const percentages = {
-    left: (full - streamed) / full,
+    left: left / full,
     streamed: streamed / full,
     withdrawn: withdrawn / full,
     available: available / full,
