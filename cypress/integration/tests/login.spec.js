5991ba6bc194fcf7b52d599cbd8c4fe4330a7d18
DelusionTea <DelusionKind@gmail.com>
2022-03-28 12:59:26 -0700

    Complite Login. Create User In progress

--- cypress/integration/tests/login.spec.js
+++ cypress/integration/tests/login.spec.js
@@ -1,6 +1,10 @@
 /// <reference types="cypress" />
-// login.spec.js created with Cypress
+// untitled.spec.js created with Cypress
 //
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
 context('Viewport', () => {
     beforeEach(() => {
         cy.visit('https://test.app-v2.roke.to/#/authorize')
@@ -10,7 +14,15 @@ it('displays two todo items by default', () => {
     cy.visit('https://test.app-v2.roke.to/#/authorize');
     cy.get('.h-screen').click();
     cy.get('.inline-flex').click();
-    cy.url().should('contains', 'https://wallet.testnet.near.org/login/');
-    
+    //cy.url().should('contains', 'https://wallet.testnet.near.org/login/');
+    cy.wait(6000);
+    cy.get('.blue').click();
+    //cy.contains('Next').rightclick();
+    cy.wait(6000);
+    cy.get('#app-container').click();
+    cy.get('.blue').click();
+    cy.wait(6000);
+    //cy.contains('Connect').rightclick();
+    cy.url().should('contains', 'https://test.app-v2.roke.to/');
   })
 })
