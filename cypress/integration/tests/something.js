544871346a9a52b2af7595ecd409d35e26dde2d1
DelusionTea <DelusionKind@gmail.com>
2022-03-28 12:58:43 -0700

    Complite Login test. in progress User creation

--- cypress/integration/tests/something.js
+++ cypress/integration/tests/something.js
@@ -1,3 +1,4 @@
+//import * as nearAPI from "near-api-js";
 const nearAPI = require("near-api-js");
 
 const { KeyPair, keyStores } = require("near-api-js");
@@ -7,17 +8,18 @@ const homedir = require("os").homedir();
 const ACCOUNT_ID = "near-example.testnet";  // NEAR account tied to the keyPair
 const NETWORK_ID = "testnet";
 // path to your custom keyPair location (ex. function access key for example account)
-const KEY_PATH = '/.near-credentials/near-example-testnet/get_token_price.json';
+//const KEY_PATH = '/.near-credentials/near-example-testnet/get_token_price.json';
+// const KEY_PATH = '\.near-credentials\near-example-testnet\get_token_price.json';
 
-const credentials = JSON.parse(fs.readFileSync(homedir + KEY_PATH));
-const keyStore = new keyStores.InMemoryKeyStore();
-keyStore.setKey(NETWORK_ID, ACCOUNT_ID, KeyPair.fromString(credentials.private_key));
+// const credentials = JSON.parse(fs.readFileSync(homedir + KEY_PATH));
+// const keyStore = new keyStores.InMemoryKeyStore();
+// keyStore.setKey(NETWORK_ID, ACCOUNT_ID, KeyPair.fromString(credentials.private_key));
 
 const { connect } = nearAPI;
 
 const config = {
   networkId: "testnet",
-  keyStore, // optional if not signing transactions
+ // keyStore, // optional if not signing transactions
   nodeUrl: "https://rpc.testnet.near.org",
   walletUrl: "https://wallet.testnet.near.org",
   helperUrl: "https://helper.testnet.near.org",
@@ -25,6 +27,7 @@ const config = {
 };
 
 // creates a new account using funds from the account used to create it
+const start = async function(){
 const near = await connect(config);
 const account = await near.account("example-account.testnet");
 await account.createAccount(
@@ -32,6 +35,7 @@ await account.createAccount(
   "8hSHprDq2StXwMtNd43wDTXQYsjXcD4MJTXQYsjXcc", // public key for new account
   "10000000000000000000" // initial balance for new account in yoctoNEAR
 );
+}
 
 const signIn = () => {
     wallet.requestSignIn(
@@ -43,8 +47,11 @@ const signIn = () => {
   };
 
 //Delete after tests
-
+if(wallet.isSignedIn()) {
+    //echo ("Something");
+}
 // deletes account found in the `account` object
 // transfers remaining account balance to the accountId passed as an argument
-
-await account.deleteAccount("example-account2.testnet");
+const end = async function(){
+     await account.deleteAccount("example-account2.testnet");
+}
