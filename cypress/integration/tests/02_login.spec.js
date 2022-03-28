8cedee36fcc1b2552914cb7d78b93600e2c72808
DelusionTea <DelusionKind@gmail.com>
2022-03-28 15:49:34 -0700

    add Login with pass phrase

--- cypress/integration/tests/02_login.spec.js
+++ cypress/integration/tests/02_login.spec.js
@@ -1,28 +1,27 @@
 /// <reference types="cypress" />
-// untitled.spec.js created with Cypress
-//
-// Start writing your Cypress tests below!
-// If you're unfamiliar with how Cypress works,
-// check out the link below and learn how to write your first test:
-// https://on.cypress.io/writing-first-test
+
 context('Viewport', () => {
-    beforeEach(() => {
-        cy.visit('https://test.app-v2.roke.to/#/authorize')
-      }
+  beforeEach(() => {
+      cy.visit('https://test.app-v2.roke.to/#/authorize')
+    }
 )
-it('displays two todo items by default', () => {
-    cy.visit('https://test.app-v2.roke.to/#/authorize');
-    cy.get('.h-screen').click();
-    cy.get('.inline-flex').click();
-    //cy.url().should('contains', 'https://wallet.testnet.near.org/login/');
-    cy.wait(6000);
-    cy.get('.blue').click();
-    //cy.contains('Next').rightclick();
-    cy.wait(6000);
-    cy.get('#app-container').click();
-    cy.get('.blue').click();
-    cy.wait(6000);
-    //cy.contains('Connect').rightclick();
-    cy.url().should('contains', 'https://test.app-v2.roke.to/');
-  })
+it('login with Pass phrase', () => {
+  cy.visit('https://test.app-v2.roke.to/#/authorize');
+  cy.get('.h-screen').click();
+  cy.get('.inline-flex').click();
+  cy.wait(6000);
+  cy.get('.account-selector > .gray-blue').click({force: true});
+  cy.url().should('contains', 'https://wallet.testnet.near.org/recover-account');
+  cy.get('[data-test-id="recoverAccountWithPassphraseButton"]').click();
+  cy.get('input').click();
+  cy.get('input').type('aunt service decline pelican actual kitchen size exhaust great prosper old attract');
+  cy.get('#app-container').click();
+  cy.get('.sc-bdvvtL').click();
+  cy.wait(6000);
+  cy.get('.blue').click();
+  cy.wait(6000);
+  cy.get('.button-group > .blue').click();
+  cy.wait(6000);
+  cy.url().should('contains', 'https://test.app-v2.roke.to/');
+})
 })
