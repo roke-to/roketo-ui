4a6ea63a4d4708178756b1f454a03e930470706a
DelusionTea <DelusionKind@gmail.com>
2022-03-28 15:57:33 -0700

    edit login. Main wallet page

--- cypress/integration/tests/02_login.spec.js
+++ cypress/integration/tests/02_login.spec.js
@@ -10,7 +10,9 @@ it('login with Pass phrase', () => {
   cy.get('.h-screen').click();
   cy.get('.inline-flex').click();
   cy.wait(6000);
-  cy.get('.account-selector > .gray-blue').click({force: true});
+  //redirect to wallet
+  cy.get('.buttons > .link').click({force: true});
+  //cy.get('.account-selector > .gray-blue').click({force: true});
   cy.url().should('contains', 'https://wallet.testnet.near.org/recover-account');
   cy.get('[data-test-id="recoverAccountWithPassphraseButton"]').click();
   cy.get('input').click();
