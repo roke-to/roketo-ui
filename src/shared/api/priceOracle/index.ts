efc96aab87ba460fb9966e25d8dba6c6f97b89a2
katarinish <katuha.p97@gmail.com>
2022-03-29 13:32:24 +0700

    formatting token price to usd

--- src/shared/api/priceOracle/index.ts
+++ src/shared/api/priceOracle/index.ts
@@ -1,17 +1,21 @@
+import numbro from 'numbro';
 import * as nearAPI from 'near-api-js';
 
-import {TokenPriceCollection, TokenPriceRaw} from './interfaces/entites';
-import {PriceOracleContract} from './interfaces/contract';
+import {TokenPriceCollection, TokenMultiplierMap, TokenPriceRaw, TokenId} from './interfaces/entites';
 
+import {PriceOracleContract} from './interfaces/contract';
 import {
   CONTRACT_CHANGE_METHODS_LIST,
   CONTRACT_VIEW_METHODS_LIST,
   ORACLE_SOURCE_ID,
   PRICE_ORACLE_CONTRACT_NAME,
+  TOKEN_MULTIPLIER_MAP,
+  DECIMAL,
 } from './constants';
 
 export interface PriceOracle {
   tokenMap: TokenPriceCollection,
+  getPriceInUsd: (tokenId: TokenId, amount: number | string) => number,
 }
 
 const convertRawPriceToTokenMap = (
@@ -20,7 +24,9 @@ const convertRawPriceToTokenMap = (
   const priceTokenMap = tokenPrices.reduce((accumulatorMap, priceToken) => {
     const [tokenId, {reports}] = priceToken;
 
-    const desiredSourceOracle = reports.find(({oracle_id}) => oracle_id === relyingSourceOracleId);
+    const desiredSourceOracle = reports.find(
+      ({oracle_id}) => oracle_id === relyingSourceOracleId
+    );
     if (!desiredSourceOracle) {
       return accumulatorMap;
     }
@@ -34,11 +40,40 @@ const convertRawPriceToTokenMap = (
   return priceTokenMap;
 };
 
-export const initPriceOracle = async (
-  {walletConnection}: {walletConnection: nearAPI.WalletConnection}
-): Promise<PriceOracle> => {
-  const account = await walletConnection.account();
 
+/**
+ * 5 NEAR = 5 * 10**24 "wrap.near"
+ * 50 DAI = 50 * 10**18 "dai.bridge.near"
+ *
+ * Price NEAR { multiplier: 1000, decimals: 26 }
+ * 5 NEAR in USD = 5 * 10**24 * 1000 / 10**(26 - 18) = 50 * 10**18
+ * Price DAI { multiplier: 101, decimals: 20 }
+ * 50 DAI in USD = 50 * 10**18 * 101 / 10**(20 - 18) = 505 * 10**17
+ *
+ * more: https://github.com/NearDeFi/price-oracle/blob/c2a10765a629dd013eeaa0f49d5631cbc0470b76/src/utils.rs#L18
+ */
+const convertTokenToUsd = (
+  priceTokenMap: TokenPriceCollection, tokenToMultiplierMap: TokenMultiplierMap
+) => (tokenId: TokenId, amount: number | string): number => {
+  const tokenPrice = priceTokenMap[tokenId];
+  const tokenMultiplier = tokenToMultiplierMap[tokenId];
+
+  if (!tokenPrice || !tokenMultiplier) {
+    return 0;
+  }
+
+  const {multiplier, decimals} = tokenPrice;
+
+  return numbro(amount)
+    .multiply(tokenMultiplier)
+    .multiply(Number(multiplier))
+    .divide(DECIMAL ** decimals)
+    .value();
+};
+
+export const initPriceOracle = async (
+  {account}: {account: nearAPI.ConnectedWalletAccount}
+): Promise<PriceOracle> => {
   const priceOracleContract = new nearAPI.Contract(account, PRICE_ORACLE_CONTRACT_NAME, {
     viewMethods: CONTRACT_VIEW_METHODS_LIST,
     changeMethods: CONTRACT_CHANGE_METHODS_LIST,
@@ -47,7 +82,10 @@ export const initPriceOracle = async (
   const rawAssets = await priceOracleContract.get_assets({});
   const tokenMap = convertRawPriceToTokenMap(rawAssets, ORACLE_SOURCE_ID);
 
+  const getPriceInUsd = convertTokenToUsd(tokenMap, TOKEN_MULTIPLIER_MAP);
+
   return {
     tokenMap,
+    getPriceInUsd,
   };
 };
