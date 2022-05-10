3d23187a1cb0244807e07f901d6d09bd1671d31f
Serge K Lebedev <hello@sergelebe.dev>
2022-05-10 17:59:21 +0600

    TRIVIAL: Add Initialized filter

--- src/features/filtering/useStreamFilters.ts
+++ src/features/filtering/useStreamFilters.ts
@@ -16,6 +16,7 @@ const STREAM_TYPE_FILTER = {
 
 const STREAM_STATUS_FILTER = {
   ALL: 'All',
+  [STREAM_STATUS.Initialized]: 'Initialized',
   [STREAM_STATUS.Active]: 'Active',
   [STREAM_STATUS.Paused]: 'Paused',
   [STREAM_STATUS.Finished]: 'Finished',
@@ -27,6 +28,7 @@ export function useStreamFilters(streams: RoketoStream[] | undefined) {
   const statusOptions = useMemo(
     () => ({
       [STREAM_STATUS_FILTER.ALL]: () => true,
+      [STREAM_STATUS_FILTER[STREAM_STATUS.Initialized]]: (stream: RoketoStream) => stream.status === STREAM_STATUS.Initialized,
       [STREAM_STATUS_FILTER[STREAM_STATUS.Active]]: (stream: RoketoStream) => stream.status === STREAM_STATUS.Active,
       [STREAM_STATUS_FILTER[STREAM_STATUS.Paused]]: (stream: RoketoStream) => stream.status === STREAM_STATUS.Paused,
       [STREAM_STATUS_FILTER[STREAM_STATUS.Finished]]: isDead,
