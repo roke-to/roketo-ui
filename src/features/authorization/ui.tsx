af1f97ab3d2c86edbba87570e665240914d93036
Serge K Lebedev <hello@sergelebe.dev>
2022-05-11 23:06:43 +0600

    ROKETO-85: Extract profile widget

--- src/features/authorization/ui.tsx
+++ src/features/authorization/ui.tsx
@@ -1,27 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import {useRoketoContext} from 'app/roketo-context';
-import {useUser} from 'shared/api/roketo-web';
-import { DropdownOpener } from 'shared/kit/DropdownOpener';
-import { DropdownMenu } from 'shared/kit/DropdownMenu';
-import { ProfileForm } from 'widgets/profile-form';
+import { Profile } from 'widgets/profile';
 
 import { Button } from '@ui/components/Button';
 import {LogoutIcon} from '@ui/icons/LogOut';
 
-import {env} from 'shared/config';
 import { testIds } from 'shared/constants';
 
 import styles from './index.module.scss';
 import { Notifications } from './Notifications';
 
 export const Authorization = () => {
-  const [isDropdownOpened, setIsDropdownOpened] = useState(false);
   const {auth} = useRoketoContext();
-  const userSWR = useUser();
-
-  const {name, email} = userSWR.data ?? {};
-  const {accountId} = auth;
   const {login, logout, signedIn} = auth;
 
   if (!signedIn) {
@@ -35,31 +26,7 @@ export const Authorization = () => {
 
   return (
     <div className={styles.root}>
-      <div className={styles.profile}>
-        <DropdownOpener
-          onChange={setIsDropdownOpened}
-          className={styles.dropdownOpener}
-          opened={isDropdownOpened}
-        >
-          <span className={styles.name}>
-            {name || accountId}
-          </span>
-
-          <img
-            className={styles.avatar}
-            src={`${env.WEB_API_URL}/users/${accountId}/avatar?email=${email}`}
-            alt=""
-          />
-        </DropdownOpener>
-
-        <DropdownMenu
-          opened={isDropdownOpened}
-          onClose={() => setIsDropdownOpened(false)}
-          className={styles.dropdownMenu}
-        >
-          <ProfileForm />
-        </DropdownMenu>
-      </div>
+      <Profile />
 
       <Notifications />
 
