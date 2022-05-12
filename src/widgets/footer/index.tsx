555eebc7869bf9bbd8364a3b5a02f76e68c098d0
katarinish <katuha.p97@gmail.com>
2022-05-12 21:09:06 +0700

    ROKETO-95: changed links to correct domen

--- src/widgets/footer/index.tsx
+++ src/widgets/footer/index.tsx
@@ -11,95 +11,91 @@ import PrivacyPolicyDoc from './assets/Privacy_policy.pdf';
 
 import styles from './styles.module.scss';
 
-export const Footer = () => {
-  console.log('Footer==>>');
+export const Footer = () => (
+  <footer className={styles.root}>
+    <Layout className={styles.layout}>
+      <section className={styles.section}>
+        <Logo className={styles.logo} type={DisplayType.CURRENT_COLOR}/>
 
-  return (
-    <footer className={styles.root}>
-      <Layout className={styles.layout}>
-        <section className={styles.section}>
-          <Logo className={styles.logo} type={DisplayType.CURRENT_COLOR} />
+        <div className={styles.links}>
+          <a
+            href="https://www.roke.to/#page-top"
+            target="_blank" rel="noreferrer"
+          >
+            About
+          </a>
+          <a
+            href="https://www.roke.to/#how-work"
+            target="_blank" rel="noreferrer"
+          >
+            Features
+          </a>
+          <a
+            href="https://www.roke.to/#used-technologies"
+            target="_blank" rel="noreferrer"
+          >
+            Docs
+          </a>
+          <a
+            href="https://www.roke.to/#team"
+            target="_blank" rel="noreferrer"
+          >
+            Team
+          </a>
+          <a
+            href="https://www.roke.to/#blog"
+            target="_blank" rel="noreferrer"
+          >
+            Blog
+          </a>
+          <a
+            href="https://kikimora-labs.notion.site/Jobs-68acb030fcac42a18ae7c62a56779d5b"
+            target="_blank" rel="noreferrer"
+          >
+            Jobs
+          </a>
+        </div>
+      </section>
 
-          <div className={styles.links}>
-            <a
-              href='https://roke-to.github.io/roke.to/#page-top'
-              target='_blank' rel='noreferrer'
-            >
-              About
-            </a>
-            <a
-              href='https://roke-to.github.io/roke.to/#how-work'
-              target='_blank' rel='noreferrer'
-            >
-              Features
-            </a>
-            <a
-              href='https://roke-to.github.io/roke.to/#used-technologies'
-              target='_blank' rel='noreferrer'
-            >
-              Docs
-            </a>
-            <a
-              href='https://roke-to.github.io/roke.to/#team'
-              target='_blank' rel='noreferrer'
-            >
-              Team
-            </a>
-            <a
-              href='https://roke-to.github.io/roke.to/#blog'
-              target='_blank' rel='noreferrer'
-            >
-              Blog
-            </a>
-            <a
-              href='https://kikimora-labs.notion.site/Jobs-68acb030fcac42a18ae7c62a56779d5b'
-              target='_blank' rel='noreferrer'
-            >
-              Jobs
-            </a>
-          </div>
-        </section>
+      <section className={styles.section}>
+        <div className={styles.legal}>
+          <a
+            href={PrivacyPolicyDoc}
+            target="_blank" rel="noreferrer"
+          >
+            Privacy Policy
+          </a>
+        </div>
 
-        <section className={styles.section}>
-          <div className={styles.legal}>
-            <a
-              href={PrivacyPolicyDoc}
-              target='_blank' rel='noreferrer'
-            >
-              Privacy Policy
-            </a>
-          </div>
+        <div className={styles.socials}>
+          <a
+            href="https://twitter.com/roketostream"
+            target="_blank" rel="noreferrer"
+          >
+            <TwitterLogo/>
+          </a>
+          <a
+            href="https://t.me/roketochat"
+            target="_blank" rel="noreferrer"
+          >
+            <TelegramLogo/>
+          </a>
+          <a
+            href="https://github.com/roke-to"
+            target="_blank" rel="noreferrer"
+          >
+            <GithubLogo/>
+          </a>
+        </div>
+      </section>
 
-          <div className={styles.socials}>
-            <a
-              href='https://twitter.com/roketostream'
-              target='_blank' rel='noreferrer'
-            >
-              <TwitterLogo />
-            </a>
-            <a
-              href='https://t.me/roketochat'
-              target='_blank' rel='noreferrer'
-            >
-              <TelegramLogo />
-            </a>
-            <a
-              href='https://github.com/roke-to'
-              target='_blank' rel='noreferrer'
-            >
-              <GithubLogo />
-            </a>
-          </div>
-        </section>
-
-        <section className={styles.jurInfo}>
-          ROKETO LABS LTD
-          <br/>
-          Registered address: Intershore Chambers, Road Town, Tortola, British Virgin Islands
-          <br />
-          Registration number: 2089289
-        </section>
-      </Layout>
-    </footer>
-  );
-};
+      <section className={styles.jurInfo}>
+        ROKETO LABS LTD
+        <br/>
+        Registered address: Intershore Chambers, Road Town, Tortola, British Virgin Islands
+        <br/>
+        Registration number: 2089289
+      </section>
+    </Layout>
+  </footer>
+);
