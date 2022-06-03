e5d06dbdd7fb8b84db084abe0d09b9725d3b59a5
Sergey Sova <mail@sergeysova.com>
2022-06-03 23:25:37 +0400

    Fix importing css in react-datetime-picker

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,12 @@ import {defineConfig} from 'vite';
 import babel from '@rollup/plugin-babel';
 import svgr from 'vite-plugin-svgr';
 import react from '@vitejs/plugin-react';
+import { viteCommonjs, esbuildCommonjs } from '@originjs/vite-plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    viteCommonjs(),
     react(),
     svgr(),
     babel({extensions: ['.ts', '.tsx']}),
@@ -20,5 +22,14 @@ export default defineConfig({
         replacement: path.resolve('src', 'shared', 'ui'),
       },
     ],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        // https://github.com/vitejs/vite/issues/5308
+        // It's because this packages imports css by their own
+        esbuildCommonjs(['react-datetime-picker'])
+      ]
+    }
   },
 });
