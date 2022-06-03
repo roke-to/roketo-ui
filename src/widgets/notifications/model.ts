1aa304e0defc4723bb7547e745e5ba89c42eee33
Serge K Lebedev <hello@sergelebe.dev>
2022-06-04 01:49:56 +0600

    ROKETO-140: Fix marking notifications as read

--- src/widgets/notifications/model.ts
+++ src/widgets/notifications/model.ts
@@ -1,14 +1,31 @@
-import {attach} from 'effector';
+import {attach, createEvent, sample} from 'effector';
 
 import {$notifications} from '~/entities/wallet';
 
 import {notificationsApiClient} from '~/shared/api/roketo-client';
 
-export const markAllReadFx = attach({
+export const markAllRead = createEvent();
+
+const markAllReadFx = attach({
   source: $notifications,
   async effect(notifications) {
     if (notifications.length !== 0) {
       await notificationsApiClient.markAllRead();
     }
   },
+});
+
+sample({
+  clock: markAllRead,
+  target: markAllReadFx,
+});
+
+sample({
+  clock: markAllRead,
+  source: $notifications,
+  fn: (notifications) =>
+    notifications.map((notification) =>
+      notification.isRead ? notification : {...notification, isRead: true},
+    ),
+  target: $notifications,
 });
