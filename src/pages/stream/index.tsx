f98f9db65537e43813f5c442fecda956d09c849f
Dmitry Boldyrev <ribkatt@gmail.com>
2022-07-01 16:29:16 +0600

    Move stream page computations to model

--- src/pages/stream/index.tsx
+++ src/pages/stream/index.tsx
@@ -1,38 +1,38 @@
 import classNames from 'classnames';
 import copy from 'clipboard-copy';
-import {format, isPast} from 'date-fns';
-import {useGate, useStore} from 'effector-react';
+import {useGate, useStore, useStoreMap} from 'effector-react';
 import React, {useState} from 'react';
 import {Link, useParams} from 'react-router-dom';
 
-import {AddFunds, useShouldShowAddFundsButton} from '~/features/add-funds';
-import {getStreamingSpeed} from '~/features/create-stream/lib';
-import {streamViewData} from '~/features/roketo-resource';
+import {AddFunds} from '~/features/add-funds';
 import {StreamControls} from '~/features/stream-control/StreamControls';
 import {WithdrawButton} from '~/features/stream-control/WithdrawButton';
 
-import {$tokens} from '~/entities/wallet';
-
-import {STREAM_DIRECTION, STREAM_STATUS} from '~/shared/api/roketo/constants';
-import type {RoketoStream} from '~/shared/api/roketo/interfaces/entities';
-import {getAvailableToWithdraw, hasPassedCliff, isDead, isIdling} from '~/shared/api/roketo/lib';
-import {formatAmount, formatSmartly, toHumanReadableValue} from '~/shared/api/token-formatter';
 import {Badge} from '~/shared/components/Badge';
 import {PageError} from '~/shared/components/PageError';
-import {useGetStreamDirection} from '~/shared/hooks/useGetStreamDirection';
-import {useRerender} from '~/shared/hooks/useRerender';
 import {ColorDot} from '~/shared/kit/ColorDot';
 import {DropdownOpener} from '~/shared/kit/DropdownOpener';
 import {TokenImage} from '~/shared/kit/TokenImage';
-import {getRoundedPercentageRatio} from '~/shared/lib/math';
-import {getStreamLink, ROUTES_MAP} from '~/shared/lib/routing';
+import {ROUTES_MAP} from '~/shared/lib/routing';
 
 import {Layout} from '@ui/components/Layout';
 import {ProgressBar} from '@ui/components/ProgressBar';
 import {LinkIcon} from '@ui/icons/Link';
 
 import {BreadcrumbIcon} from './BreadcrumbIcon';
-import {$color, $loading, $pageError, $stream, pageGate} from './model';
+import {
+  $buttonsFlags,
+  $color,
+  $comment,
+  $link,
+  $loading,
+  $pageError,
+  $speed,
+  $stream,
+  $streamInfo,
+  $streamProgress,
+  pageGate,
+} from './model';
 import styles from './styles.module.scss';
 
 export function StreamPage() {
@@ -42,12 +42,11 @@ export function StreamPage() {
   const stream = useStore($stream);
   const pageError = useStore($pageError);
   const streamColor = useStore($color);
-
-  const isStreamTicking = Boolean(
-    stream && !isIdling(stream) && streamViewData(stream).percentages.left > 0,
-  );
-
-  useRerender(1000, isStreamTicking);
+  const comment = useStore($comment);
+  const speed = useStore($speed);
+  const link = useStore($link);
+  const buttonsFlags = useStore($buttonsFlags);
+  const remaining = useStoreMap($streamInfo, (data) => data.remaining);
 
   return (
     <div className={styles.root}>
@@ -70,7 +69,7 @@ export function StreamPage() {
             <div className={styles.left}>
               <div className={classNames(styles.tile, styles.remaining)}>
                 <span className={styles.blockTitle}>Remaining</span>
-                <span>{streamViewData(stream).timeLeft || 'Finished'}</span>
+                <span>{remaining}</span>
               </div>
               <div className={classNames(styles.tile, styles.main)}>
                 {stream.is_locked && (
@@ -78,16 +77,39 @@ export function StreamPage() {
                     Locked
                   </Badge>
                 )}
-                <StreamProgress stream={stream} />
-                {!isDead(stream) && <StreamButtons stream={stream} />}
-                <StreamSpeed stream={stream} />
-                <div className={styles.divider} />
-                <StreamComment stream={stream} />
-                <StreamCopyUrlBlock stream={stream} />
+                <StreamProgress />
+                {buttonsFlags.isAlive && (
+                  <div className={styles.buttons}>
+                    {buttonsFlags.showAddFundsButton && <AddFunds stream={stream} />}
+                    <StreamControls stream={stream} />
+                    {buttonsFlags.showWithdrawButton && <WithdrawButton stream={stream} />}
+                  </div>
+                )}
+                {speed && (
+                  <div>
+                    <span className={styles.blockTitle}>Speed</span>
+                    <div className={styles.speed}>{speed}</div>
+                  </div>
+                )}
+                {comment && (
+                  <div>
+                    <span className={styles.blockTitle}>Comment</span>
+                    <div className={styles.commentBody}>{comment}</div>
+                  </div>
+                )}
+                {link && (
+                  <div>
+                    <div className={classNames(styles.blockTitle, styles.copyTitle)}>
+                      Public link to view the stream
+                      <CopyButton stringToCopy={link} />
+                    </div>
+                    <div className={styles.link}>{link}</div>
+                  </div>
+                )}
               </div>
             </div>
             <div className={styles.right}>
-              <StreamData stream={stream} />
+              <StreamData />
             </div>
           </main>
         )}
@@ -96,38 +118,34 @@ export function StreamPage() {
   );
 }
 
-function StreamProgress({stream}: {stream: RoketoStream}) {
-  const tokens = useStore($tokens);
-
-  const token = tokens[stream.token_account_id];
-  if (!token) return null;
-  const {meta} = token;
-  const {progress, percentages} = streamViewData(stream);
-
-  const streamed = Number(toHumanReadableValue(meta.decimals, progress.streamed, 3));
-  const withdrawn = Number(toHumanReadableValue(meta.decimals, progress.withdrawn, 3));
-  const streamedText = formatSmartly(streamed);
-  const withdrawnText = formatSmartly(withdrawn);
-  const streamedPercentage = getRoundedPercentageRatio(progress.streamed, progress.full, 1);
-  const withdrawnPercentage = getRoundedPercentageRatio(progress.withdrawn, progress.streamed, 1);
-
-  const progressText = `${meta.symbol} ${formatAmount(
-    meta.decimals,
-    progress.streamed,
-  )} of ${formatAmount(meta.decimals, progress.full)}`;
+function StreamProgress() {
+  const {
+    active,
+    tokenAccountId,
+    progressText,
+    streamedText,
+    withdrawnText,
+    streamedPercentage,
+    withdrawnPercentage,
+    cliffPercent,
+    withdrawn,
+    streamed,
+    total,
+  } = useStore($streamProgress);
+  if (!active) return null;
 
   return (
     <div>
       <div className={styles.numericProgress}>
-        <TokenImage tokenAccountId={stream.token_account_id} className={styles.tokenIcon} />
+        <TokenImage tokenAccountId={tokenAccountId} className={styles.tokenIcon} />
         <span>{progressText}</span>
       </div>
       <ProgressBar
         className={styles.progressBar}
-        total={progress.full}
-        streamed={progress.streamed}
-        withdrawn={progress.withdrawn}
-        cliffPercent={percentages.cliff}
+        total={total}
+        streamed={streamed}
+        withdrawn={withdrawn}
+        cliffPercent={cliffPercent}
         withBigCliffMark
       />
       <div className={styles.streamLegend}>
@@ -149,77 +167,11 @@ function StreamProgress({stream}: {stream: RoketoStream}) {
   );
 }
 
-function StreamButtons({stream}: {stream: RoketoStream}) {
-  const shouldShowAddFundsButton = useShouldShowAddFundsButton(stream);
-  const direction = useGetStreamDirection(stream);
-
-  return (
-    <div className={styles.buttons}>
-      {shouldShowAddFundsButton && <AddFunds stream={stream} />}
-      <StreamControls stream={stream} />
-
-      {direction === STREAM_DIRECTION.IN && stream.status === STREAM_STATUS.Active && (
-        <WithdrawButton stream={stream} />
-      )}
-    </div>
-  );
-}
-
-function StreamSpeed({stream}: {stream: RoketoStream}) {
-  const tokens = useStore($tokens);
-
-  const token = tokens[stream.token_account_id];
-  if (!token) return null;
-
-  return (
-    <div>
-      <span className={styles.blockTitle}>Speed</span>
-      <div className={styles.speed}>{getStreamingSpeed(Number(stream.tokens_per_sec), token)}</div>
-    </div>
-  );
-}
-
-function StreamComment({stream}: {stream: RoketoStream}) {
-  let comment: string | void;
-
-  try {
-    const parsedDescription = JSON.parse(stream.description);
-    comment = parsedDescription.comment ?? parsedDescription.c;
-  } catch {
-    comment = stream.description;
-  }
-
-  if (!comment) {
-    return null;
-  }
-
-  return (
-    <div>
-      <span className={styles.blockTitle}>Comment</span>
-      <div className={styles.commentBody}>{comment}</div>
-    </div>
-  );
-}
-
 function CopyButton({stringToCopy}: {stringToCopy: string}) {
   return (
     <button type="button" className={styles.copyButton} onClick={() => copy(stringToCopy)}>
       <LinkIcon className={styles.linkIcon} />
     </button>
-  );
-}
-
-function StreamCopyUrlBlock({stream}: {stream: RoketoStream}) {
-  const link = getStreamLink(stream.id);
-
-  return (
-    <div>
-      <div className={classNames(styles.blockTitle, styles.copyTitle)}>
-        Public link to view the stream
-        <CopyButton stringToCopy={link} />
-      </div>
-      <div className={styles.link}>{link}</div>
-    </div>
   );
 }
 
@@ -232,61 +184,59 @@ function InfoRow({title, children}: {title: string; children: React.ReactNode}) 
   );
 }
 
-function StreamData({stream}: {stream: RoketoStream}) {
-  const direction = useGetStreamDirection(stream);
-  const tokens = useStore($tokens);
-  const {
-    streamEndTimestamp,
-    cliffEndTimestamp,
-    timeLeft,
-    progress: {streamed, left, full},
-  } = streamViewData(stream);
-  const streamedToTotalPercentageRatio = getRoundedPercentageRatio(streamed, full).toNumber();
-  const leftToTotalPercentageRatio = getRoundedPercentageRatio(left, full).toNumber();
-  const available = getAvailableToWithdraw(stream).toNumber();
+function StreamData() {
   const [showOtherInfo, setShowOtherInfo] = useState(false);
-  const token = tokens[stream.token_account_id];
-  if (!token) return null;
-  const {meta} = token;
-
+  const {
+    active,
+    sender,
+    receiver,
+    amount,
+    tokenSymbol,
+    tokenName,
+    streamId,
+    created,
+    cliff,
+    end,
+    remaining,
+    transferred,
+    streamedToTotalPercentageRatio,
+    showLatestWithdrawal,
+    latestWithdrawal,
+    tokensLeft,
+    leftToTotalPercentageRatio,
+    tokensAvailable,
+  } = useStore($streamInfo);
+  if (!active) return null;
   return (
     <div className={classNames(styles.tile, styles.infoTile)}>
       <div className={styles.blockHeader}>Stream info</div>
       <InfoRow title="Sender">
-        <span className={classNames(styles.font16, styles.name)}>
-          {direction === STREAM_DIRECTION.OUT ? 'You' : stream.owner_id}
-        </span>
+        <span className={classNames(styles.font16, styles.name)}>{sender}</span>
       </InfoRow>
       <InfoRow title="Receiver">
-        <span className={classNames(styles.font16, styles.name)}>
-          {direction === STREAM_DIRECTION.IN ? 'You' : stream.receiver_id}
-        </span>
+        <span className={classNames(styles.font16, styles.name)}>{receiver}</span>
       </InfoRow>
       <InfoRow title="Amount">
         <span className={styles.font14}>
-          {formatAmount(meta.decimals, full)}&nbsp;
-          <span className={styles.font12}>{meta.symbol}</span>
+          {amount}&nbsp;<span className={styles.font12}>{tokenSymbol}</span>
         </span>
       </InfoRow>
       <InfoRow title="Stream Created">
-        <span className={styles.font14}>
-          {format(new Date(Number(stream.timestamp_created) / 1000000), "PP 'at' p")}
-        </span>
+        <span className={styles.font14}>{created}</span>
       </InfoRow>
-      {cliffEndTimestamp && (
-        <InfoRow title={isPast(cliffEndTimestamp) ? 'Cliff Period Ended' : 'Cliff Period Ends'}>
-          <span className={styles.font14}>{format(cliffEndTimestamp, "PP 'at' p")}</span>
+      {cliff && (
+        <InfoRow title={cliff.title}>
+          <span className={styles.font14}>{cliff.value}</span>
         </InfoRow>
       )}
-      {streamEndTimestamp && (
-        <InfoRow title={isPast(streamEndTimestamp) ? 'Stream Ended' : 'Stream Ends'}>
-          <span className={styles.font14}>{format(streamEndTimestamp, "PP 'at' p")}</span>
+      {end && (
+        <InfoRow title={end.title}>
+          <span className={styles.font14}>{end.value}</span>
         </InfoRow>
       )}
       <InfoRow title="Token">
         <span className={styles.font14}>
-          {meta.name},&nbsp;
-          <span className={styles.font12}>{meta.symbol}</span>
+          {tokenName},&nbsp;<span className={styles.font12}>{tokenSymbol}</span>
         </span>
       </InfoRow>
 
@@ -305,41 +255,39 @@ function StreamData({stream}: {stream: RoketoStream}) {
         <>
           <InfoRow title="Stream ID">
             <div className={styles.centeredFlex}>
-              <span className={classNames(styles.font14, styles.streamID)}>{stream.id}</span>
-              <CopyButton stringToCopy={stream.id} />
+              <span className={classNames(styles.font14, styles.streamID)}>{streamId}</span>
+              <CopyButton stringToCopy={streamId} />
             </div>
           </InfoRow>
           <InfoRow title="Remaining">
-            <span className={styles.font14}>{timeLeft || 'Finished'}</span>
+            <span className={styles.font14}>{remaining}</span>
           </InfoRow>
           <InfoRow title="Tokens Transferred">
             <span className={styles.font14}>
-              {formatAmount(meta.decimals, streamed)}&nbsp;
+              {transferred}&nbsp;
               <span className={styles.font12}>
-                {meta.symbol}{' '}
+                {tokenSymbol}{' '}
                 <span className={styles.grey}>({streamedToTotalPercentageRatio}%)</span>
               </span>
             </span>
           </InfoRow>
-          {stream.timestamp_created !== stream.last_action && (
+          {showLatestWithdrawal && (
             <InfoRow title="Latest Withdrawal">
-              <span className={styles.font14}>
-                {format(new Date(Number(stream.last_action) / 1000000), "PP 'at' p")}
-              </span>
+              <span className={styles.font14}>{latestWithdrawal}</span>
             </InfoRow>
           )}
           <InfoRow title="Tokens Left">
             <span className={styles.font14}>
-              {formatAmount(meta.decimals, left)}&nbsp;
+              {tokensLeft}&nbsp;
               <span className={styles.font12}>
-                {meta.symbol} <span className={styles.grey}>({leftToTotalPercentageRatio}%)</span>
+                {tokenSymbol} <span className={styles.grey}>({leftToTotalPercentageRatio}%)</span>
               </span>
             </span>
           </InfoRow>
           <InfoRow title="Tokens Available">
             <span className={styles.font14}>
-              {hasPassedCliff(stream) ? formatAmount(meta.decimals, available) : '0'}&nbsp;
-              <span className={styles.font12}>{meta.symbol}</span>
+              {tokensAvailable}&nbsp;
+              <span className={styles.font12}>{tokenSymbol}</span>
             </span>
           </InfoRow>
         </>
--- src/pages/stream/model.ts
+++ src/pages/stream/model.ts
@@ -1,11 +1,29 @@
-import {combine, createEffect, createStore, sample} from 'effector';
+import {BigNumber} from 'bignumber.js';
+import {format, isPast} from 'date-fns';
+import {combine, createEffect, createEvent, createStore, sample, split} from 'effector';
 import {createGate} from 'effector-react';
 
-import {$roketoWallet, lastCreatedStreamUpdated} from '~/entities/wallet';
+import {getStreamingSpeed} from '~/features/create-stream/lib';
+import {streamViewData} from '~/features/roketo-resource';
+
+import {$accountId, $roketoWallet, $tokens, lastCreatedStreamUpdated} from '~/entities/wallet';
 
 import {getStream} from '~/shared/api/methods';
+import {STREAM_DIRECTION, STREAM_STATUS} from '~/shared/api/roketo/constants';
 import type {RoketoStream} from '~/shared/api/roketo/interfaces/entities';
+import {
+  getAvailableToWithdraw,
+  getStreamDirection,
+  hasPassedCliff,
+  isDead,
+  isIdling,
+  isLocked,
+} from '~/shared/api/roketo/lib';
+import {formatAmount, formatSmartly, toHumanReadableValue} from '~/shared/api/token-formatter';
+import {RichToken} from '~/shared/api/types';
+import {getRoundedPercentageRatio} from '~/shared/lib/math';
 import {createProtectedEffect} from '~/shared/lib/protectedEffect';
+import {getStreamLink} from '~/shared/lib/routing';
 
 export const pageGate = createGate<string | null>({defaultState: null});
 export const $stream = createStore<RoketoStream | null>(null);
@@ -27,6 +45,68 @@ export const $color = $stream.map((stream): string | null => {
     return null;
   }
 });
+
+const dataUpdated = createEvent<{
+  stream: RoketoStream;
+  token: RichToken;
+}>();
+const drawRetriggered = createEvent<{
+  stream: RoketoStream;
+  token: RichToken;
+}>();
+const noData = createEvent<unknown>();
+
+export const $comment = createStore<string | null>(null);
+export const $speed = createStore<string | null>(null);
+export const $link = createStore<string | null>(null);
+
+export const $streamProgress = createStore({
+  active: false,
+  tokenAccountId: '',
+  progressText: '',
+  streamedText: '',
+  withdrawnText: '',
+  streamedPercentage: new BigNumber(0),
+  withdrawnPercentage: new BigNumber(0),
+  cliffPercent: null as number | null,
+  withdrawn: '',
+  streamed: '',
+  total: '',
+});
+
+export const $streamInfo = createStore({
+  active: false,
+  sender: '',
+  receiver: '',
+  amount: '',
+  tokenSymbol: '',
+  tokenName: '',
+  streamId: '',
+  created: '',
+  cliff: null as null | {title: string; value: string},
+  end: null as null | {title: string; value: string},
+  remaining: '',
+  transferred: '',
+  streamedToTotalPercentageRatio: 0,
+  showLatestWithdrawal: false,
+  latestWithdrawal: '',
+  tokensLeft: '',
+  leftToTotalPercentageRatio: 0,
+  tokensAvailable: '',
+});
+
+export const $buttonsFlags = createStore({
+  isAlive: false,
+  showAddFundsButton: false,
+  showWithdrawButton: false,
+});
+
+const progressRedrawTimerFx = createEffect(
+  () =>
+    new Promise<void>((rs) => {
+      setTimeout(rs, 1000);
+    }),
+);
 
 const streamRevalidationTimerFx = createEffect(
   () =>
@@ -77,3 +157,169 @@ sample({
   fn: () => null,
   target: $pageError,
 });
+
+split({
+  source: combine($stream, $pageError, $tokens, (stream, pageError, tokens) => {
+    if (pageError || !stream || !tokens[stream.token_account_id]) return null;
+    return {
+      stream,
+      token: tokens[stream.token_account_id],
+    };
+  }),
+  match: (upd) => (upd ? 'dataUpdated' : 'noData'),
+  cases: {
+    dataUpdated,
+    noData,
+  },
+});
+
+sample({
+  clock: [dataUpdated, drawRetriggered],
+  fn({token: {meta}, stream}) {
+    const {progress, percentages} = streamViewData(stream);
+    return {
+      active: true,
+      tokenAccountId: stream.token_account_id,
+      progressText: `${meta.symbol} ${formatAmount(
+        meta.decimals,
+        progress.streamed,
+      )} of ${formatAmount(meta.decimals, progress.full)}`,
+      streamedText: formatSmartly(
+        Number(toHumanReadableValue(meta.decimals, progress.streamed, 3)),
+      ),
+      withdrawnText: formatSmartly(
+        Number(toHumanReadableValue(meta.decimals, progress.withdrawn, 3)),
+      ),
+      streamedPercentage: getRoundedPercentageRatio(progress.streamed, progress.full, 1),
+      withdrawnPercentage: getRoundedPercentageRatio(progress.withdrawn, progress.streamed, 1),
+      cliffPercent: percentages.cliff,
+      withdrawn: progress.withdrawn,
+      streamed: progress.streamed,
+      total: progress.full,
+    };
+  },
+  target: $streamProgress,
+});
+
+sample({
+  clock: $stream,
+  filter: Boolean,
+  fn(stream) {
+    let comment: string | void;
+    try {
+      const parsedDescription = JSON.parse(stream.description);
+      comment = parsedDescription.comment ?? parsedDescription.c;
+    } catch {
+      comment = stream.description;
+    }
+    return comment ?? null;
+  },
+  target: $comment,
+});
+
+sample({
+  clock: [dataUpdated, drawRetriggered],
+  fn: ({stream, token}) => getStreamingSpeed(Number(stream.tokens_per_sec), token),
+  target: $speed,
+});
+
+sample({
+  clock: $stream,
+  fn: (stream) => (stream ? getStreamLink(stream.id) : null),
+  target: $link,
+});
+
+sample({
+  clock: [dataUpdated, drawRetriggered],
+  source: $accountId,
+  fn(accountId, {stream, token: {meta}}) {
+    const {
+      streamEndTimestamp,
+      cliffEndTimestamp,
+      timeLeft,
+      progress: {streamed, left, full},
+    } = streamViewData(stream);
+    const streamedToTotalPercentageRatio = getRoundedPercentageRatio(streamed, full).toNumber();
+    const leftToTotalPercentageRatio = getRoundedPercentageRatio(left, full).toNumber();
+    const available = getAvailableToWithdraw(stream).toNumber();
+    const direction = getStreamDirection(stream, accountId);
+    return {
+      active: true,
+      sender: direction === STREAM_DIRECTION.OUT ? 'You' : stream.owner_id,
+      receiver: direction === STREAM_DIRECTION.IN ? 'You' : stream.receiver_id,
+      amount: formatAmount(meta.decimals, full),
+      tokenSymbol: meta.symbol,
+      tokenName: meta.name,
+      streamId: stream.id,
+      created: format(new Date(Number(stream.timestamp_created) / 1000000), "PP 'at' p"),
+      cliff: cliffEndTimestamp
+        ? {
+            title: isPast(cliffEndTimestamp) ? 'Cliff Period Ended' : 'Cliff Period Ends',
+            value: format(cliffEndTimestamp, "PP 'at' p"),
+          }
+        : null,
+      end: streamEndTimestamp
+        ? {
+            title: isPast(streamEndTimestamp) ? 'Stream Ended' : 'Stream Ends',
+            value: format(streamEndTimestamp, "PP 'at' p"),
+          }
+        : null,
+      remaining: timeLeft || 'Finished',
+      transferred: formatAmount(meta.decimals, streamed),
+      streamedToTotalPercentageRatio,
+      showLatestWithdrawal: stream.timestamp_created !== stream.last_action,
+      latestWithdrawal: format(new Date(Number(stream.last_action) / 1000000), "PP 'at' p"),
+      tokensLeft: formatAmount(meta.decimals, left),
+      leftToTotalPercentageRatio,
+      tokensAvailable: hasPassedCliff(stream) ? formatAmount(meta.decimals, available) : '0',
+    };
+  },
+  target: $streamInfo,
+});
+
+sample({
+  clock: [dataUpdated, drawRetriggered],
+  source: $accountId,
+  filter: Boolean,
+  fn(accountId, {stream}) {
+    const direction = getStreamDirection(stream, accountId);
+    const isOutgoingStream = direction === STREAM_DIRECTION.OUT;
+    return {
+      isAlive: !isDead(stream),
+      showAddFundsButton: isOutgoingStream && !isLocked(stream),
+      showWithdrawButton:
+        direction === STREAM_DIRECTION.IN && stream.status === STREAM_STATUS.Active,
+    };
+  },
+  target: $buttonsFlags,
+});
+
+/** when redraw timer ends, send actual data to retrigger event */
+sample({
+  clock: progressRedrawTimerFx.doneData,
+  source: dataUpdated,
+  target: drawRetriggered,
+});
+
+/**
+ * while page is open and stream is not complete yet,
+ * redraw progress bas each second
+ * */
+sample({
+  clock: [$stream, progressRedrawTimerFx.doneData],
+  filter: combine(
+    pageGate.status,
+    progressRedrawTimerFx.pending,
+    $stream,
+    (status, pending, stream) => {
+      if (!status || pending || !stream) return false;
+      return Boolean(stream && !isIdling(stream) && streamViewData(stream).percentages.left > 0);
+    },
+  ),
+  target: [progressRedrawTimerFx],
+});
+
+$streamProgress.reset([noData]);
+$speed.reset([noData]);
+$streamInfo.reset([noData]);
+$buttonsFlags.reset([noData]);
