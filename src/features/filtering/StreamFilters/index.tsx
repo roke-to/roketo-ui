a4f9e2b62c5c98067819b89fea96639edcc5afe0
Serge K Lebedev <hello@sergelebe.dev>
2022-07-05 17:58:15 +0600

    WIP: ROKETO-203: Implement text filtering

--- src/features/filtering/StreamFilters/index.tsx
+++ src/features/filtering/StreamFilters/index.tsx
@@ -1,13 +1,19 @@
 import classNames from 'classnames';
-import React, {useEffect, useState} from 'react';
+import {useStore} from 'effector-react';
+import React, {useEffect, useMemo, useState} from 'react';
+
+import {$accountId} from '~/entities/wallet';
 
 import type {RoketoStream} from '~/shared/api/roketo/interfaces/entities';
+import {useMediaQuery} from '~/shared/hooks/useMatchQuery';
 import {Filter, FilterOptionWithCounter} from '~/shared/kit/Filter';
 
 import {OrderType, SortIcon} from '@ui/icons/Sort';
 
 import {DirectionSorts} from '../DirectionSorts';
 import {useStreamFilters} from '../useStreamFilters';
+import {ReactComponent as Clear} from './clear.svg';
+import {ReactComponent as Magnifier} from './magnifier.svg';
 import styles from './styles.module.scss';
 
 function compareBy(a: RoketoStream, b: RoketoStream, key: keyof RoketoStream) {
@@ -44,35 +50,102 @@ type StreamFiltersProps = {
 };
 
 export function StreamFilters({items, onFilterDone, className}: StreamFiltersProps) {
+  const [showInput, setShowInput] = useState(false);
+  const [filterText, setFilterText] = useState('');
   const filter = useStreamFilters(items);
   const {filteredItems} = filter.result;
+  const accountId = useStore($accountId);
+  const isSmallForTextFilterInput = useMediaQuery('(max-width: 1111px)') && showInput;
+  const isSmallForTextFilterButton = useMediaQuery('(max-width: 767px)') && !showInput;
+  const isSmallForTextFilter = isSmallForTextFilterInput || isSmallForTextFilterButton;
 
   const [sorting, setSorting] = useState(sorts.mostRecent);
   const sortOptions = Object.values(sorts);
   const isEmptyList = items ? items.length === 0 : true;
 
+  const filteredItemsByText = useMemo(
+    () =>
+      filteredItems?.filter(({description, receiver_id, owner_id}) => {
+        const trimmedFilterText = filterText.trim();
+
+        if (!trimmedFilterText) {
+          return true;
+        }
+
+        const comment =
+          (() => {
+            try {
+              return JSON.parse(description).comment;
+            } catch {
+              return description;
+            }
+          })() ?? '';
+
+        const counterActor = accountId === owner_id ? receiver_id : owner_id;
+
+        return comment.includes(trimmedFilterText) || counterActor.match(trimmedFilterText);
+      }),
+    [filteredItems, filterText],
+  );
+
   useEffect(() => {
-    if (!filteredItems) {
-      onFilterDone(filteredItems);
+    if (!filteredItemsByText) {
+      onFilterDone(filteredItemsByText);
 
       return;
     }
 
-    const sortedStreams = [...filteredItems];
+    const sortedStreams = [...filteredItemsByText];
 
     sortedStreams.sort(sorting.fn);
 
     onFilterDone(sortedStreams);
-  }, [filteredItems, onFilterDone, sorting.fn]);
+  }, [filteredItemsByText, onFilterDone, sorting.fn]);
+
+  const textFilter = (
+    <div className={classNames(styles.textFilter, showInput && styles.withInput)} key="text-filter">
+      <Magnifier className={styles.textFilterMagnifier} />
+      <input
+        className={styles.textFilterInput}
+        value={filterText}
+        onChange={(e) => setFilterText(e.currentTarget.value)}
+        onFocus={() => setShowInput(true)}
+        onBlur={(e) => {
+          const isEmptyInput = e.currentTarget.value.trim() === '';
+          if (isEmptyInput) {
+            setShowInput(false);
+          }
+        }}
+      />
+      {filterText.trim() && (
+        <Clear
+          className={styles.textFilterClear}
+          onClick={() => {
+            setShowInput(false);
+            setFilterText('');
+          }}
+        />
+      )}
+    </div>
+  );
 
   return (
     <div className={classNames(styles.root, className)}>
+      {isSmallForTextFilter && (
+        <div style={{flexBasis: '100%', display: 'flex', justifyContent: 'center'}}>
+          {textFilter}
+        </div>
+      )}
+
       <DirectionSorts
         directions={filter.directionFilter.optionsArray}
         activeDirection={filter.directionFilter.option}
         onDirectionClick={filter.directionFilter.selectOption}
         isInactive={isEmptyList}
       />
+
+      {!isSmallForTextFilter && textFilter}
+
       <div className={styles.filtersWrapper}>
         <Filter
           options={filter.statusFilter.optionsArray}
