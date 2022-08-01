0976d1b8838c823faf4768fbbda8d91c2f9d7601
Dmitry Boldyrev <ribkatt@gmail.com>
2022-08-01 19:39:12 +0600

    Don't trigger useOutsideClick callback by interaction with modal content
    When modal is active, any other application content is "freezed" so it should not trigger changes in its own ui
    Looks like react-modal has some global state with setTimeout hacks which forces all modal instances to close in some specific conditions therefore when user click on modal, useOutsideClick in underlying application closes modal itself

--- src/shared/hooks/useOutsideClick.ts
+++ src/shared/hooks/useOutsideClick.ts
@@ -1,12 +1,15 @@
 import React from 'react';
 
 export const useOutsideClick = (ref: React.RefObject<HTMLElement>, callback: () => void) => {
-  const handleClick = ({target}: DocumentEventMap['click']) => {
+  const handleClick = (ev: DocumentEventMap['click']) => {
+    const {target} = ev;
     if (
       ref.current &&
       target instanceof HTMLElement &&
       document.documentElement.contains(target) &&
-      !ref.current.contains(target)
+      !ref.current.contains(target) &&
+      // @ts-expect-error dom typings lacks .path property
+      !detectReactModalClicks(ref.current, ev)
     ) {
       callback();
     }
@@ -20,3 +23,22 @@ export const useOutsideClick = (ref: React.RefObject<HTMLElement>, callback: () 
     };
   });
 };
+
+function detectReactModalClicks(
+  refEl: HTMLElement,
+  ev: DocumentEventMap['click'] & {path: HTMLElement[]},
+) {
+  const refPath: ParentNode[] = [refEl];
+  let current: ParentNode = refEl;
+  while (current.parentNode) {
+    current = current.parentNode;
+    refPath.push(current);
+  }
+  const targetPath = [...ev.path];
+  /** find part of the path which is different
+   * to decide whether referenced element is inside modal itself */
+  while (targetPath.length > 0 && refPath.includes(targetPath[0])) {
+    targetPath.pop();
+  }
+  return targetPath.some((el) => el.classList?.contains('ReactModalPortal'));
+}
