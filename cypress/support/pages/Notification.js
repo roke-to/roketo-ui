321d2a9f46b4d1f912862d0d4ac774d508085396
Kateryna Zhdanova <k.zhdanova@mrlab.ae>
2022-08-12 11:24:00 +0400

    test fixed

--- cypress/support/pages/Notification.js
+++ cypress/support/pages/Notification.js
@@ -12,10 +12,10 @@ class Notifications {
 
   checknew(type, receiverId) {
     const text = {
-      start: `You've successfully started a stream to ${receiverId}`,
+      start: `You've successfully created a stream to ${receiverId}`,
       pause: `The stream to ${receiverId} is paused`,
-      restart: `The stream to ${receiverId} was continued`,
-      stop: `The stream to ${receiverId} has ended`,
+      restart: `The stream to ${receiverId} was resumed`,
+      stop: `The stream to ${receiverId} has completed`,
       cliff: `The stream to ${receiverId} has passed the cliff period`,
       funds: `The funds were added to the stream to ${receiverId}`,
     }[type];
@@ -25,13 +25,13 @@ class Notifications {
 
   checkReceiver(type, senderId) {
     const text = {
-      start: `${senderId} started a stream for you to receive`,
+      start: `${senderId} created a stream to you`,
       pause: `The stream from ${senderId} is paused`,
-      restart: `${senderId} has continued the stream`,
-      stop: `The stream from ${senderId} has ended`,
+      restart: `${senderId} has resumed the stream`,
+      stop: `The stream from ${senderId} has completed`,
       cliff: `The stream from ${senderId} has passed the cliff period`,
       funds: `The funds were added to the stream from ${senderId}`,
-      due: `The stream from ${senderId} id due`,
+      due: `The stream from ${senderId} was finished`,
     }[type];
 
     cy.get(testSelectors.notificationsContainer).contains(text);
