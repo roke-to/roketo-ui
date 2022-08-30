7bde8febd546f893cec4b6d5296ab8cd3faf1216
k-zhdanova <104555497+k-zhdanova@users.noreply.github.com>
2022-08-30 19:21:21 +0400

    ROKETO-254: Add MyNearWallet to login screen + redesign (#180)
    * redesign login screen
    
    * login with myNear wallet
    
    * test fixed
    
    * small fix
    
    * removed am/pm from dateTimePicker component and test
    
    * unused icons deleted
    
    * align logo and wallets
    
    Co-authored-by: Kateryna Zhdanova <k.zhdanova@mrlab.ae>

--- src/features/authorization/ui.tsx
+++ src/features/authorization/ui.tsx
@@ -1,69 +1,62 @@
-import senderIcon from '@near-wallet-selector/sender/assets/sender-icon.png';
+import {ModuleState} from '@near-wallet-selector/core';
+import cn from 'classnames';
 import {useStore} from 'effector-react';
 import {useState} from 'react';
 
-import {$isSignedIn, loginFx, logoutFx} from '~/entities/wallet';
+import {
+  $isSignedIn,
+  $walletSelectorState,
+  loginViaWalletFx,
+  logoutFx,
+  walletClicked,
+} from '~/entities/wallet';
 
+import {resolveWalletIcon, WalletIconType} from '~/shared/api/near/options';
 import {testIds} from '~/shared/constants';
 
 import {Button} from '@ui/components/Button';
 import {LogoutIcon} from '@ui/icons/LogOut';
 
 import styles from './index.module.scss';
-import nearIcon from './near-wallet-icon.png';
-
-const SENDER_DOWNLOAD_URL =
-  'https://chrome.google.com/webstore/detail/sender-wallet/epapihdplajcdnnkdeiahlgigofloibg';
 
 export const Authorization = () => {
+  const [clickedWallet, setClickedWallet] = useState('');
   const signedIn = useStore($isSignedIn);
-  const [walletType, selectWallet] = useState<'near' | 'sender'>('near');
-  const [showSenderInstallInfo, setInstallInfo] = useState(false);
-  const isLoading = useStore(loginFx.pending);
+  const {modules} = useStore($walletSelectorState);
+  const isLoading = useStore(loginViaWalletFx.pending);
 
-  const nearText =
-    isLoading && walletType === 'near' ? 'Signing in...' : 'Sign in with NEAR Wallet';
-  let senderText = 'Sign in with Sender Wallet';
-  if (showSenderInstallInfo) {
-    senderText = 'You need to install Sender chrome extension';
-  } else if (isLoading && walletType === 'sender') {
-    senderText = 'Signing in...';
-  }
+  const handleWalletClick = (module: ModuleState) => () => {
+    setClickedWallet(module.id);
+    walletClicked(module);
+  };
 
   if (!signedIn) {
     return (
-      <>
-        <Button
-          onClick={() => {
-            selectWallet('near');
-            loginFx('near');
-          }}
-          className={styles.root}
-          testId={testIds.signInButton}
-          disabled={isLoading}
-        >
-          <img src={nearIcon} alt="NEAR Wallet" className={styles.logo} />
-          <span className={styles.name}>{nearText}</span>
-          <LogoutIcon />
-        </Button>
-        <Button
-          onClick={() => {
-            if (window.near) {
-              selectWallet('sender');
-              loginFx('sender');
-            } else {
-              window.open(SENDER_DOWNLOAD_URL, '_blank');
-              setInstallInfo(true);
-            }
-          }}
-          className={styles.root}
-          disabled={isLoading || showSenderInstallInfo}
-        >
-          <img src={senderIcon} alt="Sender Wallet" className={styles.logo} />
-          <span className={styles.name}>{senderText}</span>
-          <LogoutIcon />
-        </Button>
-      </>
+      <div className={styles.root}>
+        {modules.map((module: ModuleState) => {
+          // @ts-expect-error
+          const {name, iconUrl, available, downloadUrl} = module.metadata;
+          const clicked = module.id === clickedWallet;
+          const WalletIcon = resolveWalletIcon(iconUrl as WalletIconType);
+
+          const description = clicked && isLoading ? 'Connecting...' : name;
+
+          return (
+            <Button
+              key={module.id}
+              onClick={
+                available ? handleWalletClick(module) : () => window.open(downloadUrl, '_blank')
+              }
+              className={cn(styles.button, {[styles.isLoading]: isLoading && clicked})}
+              testId={module.id === 'near-wallet' ? testIds.signInButton : ''}
+              disabled={isLoading}
+            >
+              <img src={WalletIcon} alt={name} className={styles.logo} />
+              <span className={styles.name}>{description}</span>
+            </Button>
+          );
+        })}
+      </div>
     );
   }
 
