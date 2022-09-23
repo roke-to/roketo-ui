dfc20adf1c68dc3e28c75d01a07ea67b9277e469
Serge K Lebedev <hello@sergelebe.dev>
2022-09-23 16:14:21 +0600

    TRIVIAL: Fix useOutsideClick (#190)

--- src/shared/hooks/useOutsideClick.ts
+++ src/shared/hooks/useOutsideClick.ts
@@ -1,15 +1,13 @@
 import React from 'react';
 
 export const useOutsideClick = (ref: React.RefObject<HTMLElement>, callback: () => void) => {
-  const handleClick = (ev: DocumentEventMap['click']) => {
-    const {target} = ev;
+  const handleClick = ({target}: DocumentEventMap['click']) => {
     if (
       ref.current &&
       target instanceof HTMLElement &&
       document.documentElement.contains(target) &&
       !ref.current.contains(target) &&
-      // @ts-expect-error dom typings lacks .path property
-      !detectReactModalClicks(ref.current, ev)
+      !detectReactModalClicks(target)
     ) {
       callback();
     }
@@ -24,21 +22,8 @@ export const useOutsideClick = (ref: React.RefObject<HTMLElement>, callback: () 
   });
 };
 
-function detectReactModalClicks(
-  refEl: HTMLElement,
-  ev: DocumentEventMap['click'] & {path: HTMLElement[]},
-) {
-  const refPath: ParentNode[] = [refEl];
-  let current: ParentNode = refEl;
-  while (current.parentNode) {
-    current = current.parentNode;
-    refPath.push(current);
-  }
-  const targetPath = [...ev.path];
-  /** find part of the path which is different
-   * to decide whether referenced element is inside modal itself */
-  while (targetPath.length > 0 && refPath.includes(targetPath[0])) {
-    targetPath.pop();
-  }
-  return targetPath.some((el) => el.classList?.contains('ReactModalPortal'));
+function detectReactModalClicks(target: HTMLElement) {
+  const modals = Array.from(document.querySelectorAll('.ReactModalPortal'));
+
+  return modals.some((modal) => modal.contains(target));
 }
